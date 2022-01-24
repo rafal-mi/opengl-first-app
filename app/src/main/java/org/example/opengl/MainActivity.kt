6daafacc0eec47cc23d1aab5d16a5f573f303ef2
Rafal Mi <rafal.m4@gmail.com>
2022-01-24 21:23:21 +0100

    First red screen

---
+++ app/src/main/java/org/example/opengl/MainActivity.kt
@@ -0,0 +1,18 @@
+package org.example.opengl
+
+import android.opengl.GLSurfaceView
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var gLView: GLSurfaceView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        gLView = MyGLSurfaceView(this)
+        setContentView(gLView)
+    }
+}
