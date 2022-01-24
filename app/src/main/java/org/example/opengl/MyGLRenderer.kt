44e64bca4dd2e001ab505ec3c8f47569122f6e5a
Rafal Mi <rafal.m4@gmail.com>
2022-01-24 21:35:53 +0100

    Triangle

--- app/src/main/java/org/example/opengl/MyGLRenderer.kt
+++ app/src/main/java/org/example/opengl/MyGLRenderer.kt
@@ -7,14 +7,20 @@ import javax.microedition.khronos.opengles.GL10
 
 class MyGLRenderer : GLSurfaceView.Renderer {
 
+    private lateinit var mTriangle: Triangle
+
     override fun onSurfaceCreated(unused: GL10, config: EGLConfig) {
         // Set the background frame color
-        GLES20.glClearColor(1.0f, 0.0f, 0.0f, 1.0f)
+        //GLES20.glClearColor(1.0f, 0.0f, 0.0f, 1.0f)
+
+        mTriangle = Triangle()
     }
 
     override fun onDrawFrame(unused: GL10) {
         // Redraw background color
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+        //GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+
+        mTriangle.draw()
     }
 
     override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
